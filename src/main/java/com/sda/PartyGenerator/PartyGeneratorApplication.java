1390384dd7dea3e922aa0dc3f7dc8c78d0998cb7
Dajana <dajanatorzewska@gamil.com>
2021-01-17 08:56:01 +0100

    Initial commit

---
+++ src/main/java/com/sda/PartyGenerator/PartyGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.sda.PartyGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/PartyGenerator/PartyGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.PartyGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
